97fae09f65915c0cd2fe73cf5611b91864feac9f
ANTAR NANDI <antarnandy.2@gmail.com>
2024-06-08 14:20:36 +0600

    registration and login part partially done

--- app/Http/Controllers/Frontend/LoginController.php
+++ app/Http/Controllers/Frontend/LoginController.php
@@ -4,8 +4,31 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('frontend.pages.login');
+    }
+    public function loginSubmit(Request $request)
+    {
+        $data = $request->all();
+        // dd($data);
+        if (Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'status' => 'active'])) {
+            Session::put('user', $data['email']);
+            request()->session()->flash('success', 'Successfully login');
+            return redirect()->route('blog');
+        } else {
+            request()->session()->flash('error', 'Invalid email and password pleas try again!');
+            return redirect()->back();
+        }
+    }
+    public function logout()
+    {
+        Session::flush();
+        return redirect()->to('/');
+    }
 }
--- app/Http/Controllers/Frontend/RegistrationController.php
+++ app/Http/Controllers/Frontend/RegistrationController.php
@@ -2,10 +2,47 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class RegistrationController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('frontend.pages.registration');
+    }
+    public function signupSubmit(Request $request)
+    {
+        $this->validate($request, [
+            'first_name' => 'string|required|min:2',
+            'mobile' => 'required|min:2',
+            'email' => 'string|required|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+        $data = $request->all();
+        $check = $this->create($data);
+        Session::put('user', $data['email']);
+        Session::put('name', $data['first_name'] . " " . $data['last_name']);
+        if ($check) {
+            request()->session()->flash('success', 'Successfully Registered');
+            return redirect()->route('/');
+        } else {
+            request()->session()->flash('error', 'Please try again!');
+            return back();
+        }
+    }
+    public function create(array $data)
+    {
+        // dd($data);
+        return User::create([
+            'name' => $data['first_name'] . " " . $data['last_name'],
+            'mobile' => $data['mobile'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'status' => 'active'
+        ]);
+    }
 }
